c5ff3973ffe387509112c54376775cc57a639793
Raghunath Mustur <raghunathreddymustur321@gmail.com>
2024-05-05 14:03:29 +0530

    Reading Properties via Bean mapping

--- Version1/Readme.md
+++ Version1/Readme.md
@@ -5,6 +5,63 @@
 
 ### Different ways to Access Configuration properties
 ![img.png](img.png)
+
+Mapping to Bean - Way Three
+---------------
+#### Read multiple properties by mapping to a record
+1. Define the properties in properties file
+   ```
+   accounts:
+    message: "Welcome to EazyBank accounts related local APIs "
+    contactDetails:
+        name: "John Doe - Developer"
+        email: "john@eazybank.com"
+    onCallSupport:
+        - (555) 555-1234
+        - (555) 523-1345
+   ```
+2. Define Dto Bean 
+   ```java
+   package com.eazybytes.accounts.dto;
+   
+   import org.springframework.boot.context.properties.ConfigurationProperties;
+   
+   import java.util.List;
+   import java.util.Map;
+   
+   @ConfigurationProperties(prefix = "accounts")
+   public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+   
+   }            
+      
+   ```
+3. Enable Configuration in SpringBootApplication
+   ```java
+   @EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+   public class AccountsApplication {
+   
+       public static void main(String[] args) {
+           SpringApplication.run(AccountsApplication.class, args);
+       }
+   
+   }
+   
+   ```
+4. Read The properties by injecting
+   ```java
+
+    @Autowired
+    private AccountsContactInfoDto accountsContactInfoDto;
+    
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountsContactInfoDto> getContactInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(accountsContactInfoDto);
+    }
+    ```
+
+   
     
 
 
--- Version1/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ Version1/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -1,6 +1,7 @@
 package com.eazybytes.accounts.controller;
 
 import com.eazybytes.accounts.constants.AccountsConstants;
+import com.eazybytes.accounts.dto.AccountsContactInfoDto;
 import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.dto.ErrorResponseDto;
 import com.eazybytes.accounts.dto.ResponseDto;
@@ -44,6 +45,9 @@ public class AccountsController {
 
     @Autowired
     private Environment environment;
+
+    @Autowired
+    private AccountsContactInfoDto accountsContactInfoDto;
 
     public AccountsController(IAccountsService iAccountsService) {
         this.iAccountsService = iAccountsService;
@@ -224,5 +228,31 @@ public class AccountsController {
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
     }
-
+    @Operation(
+            summary = "Get Contact Info",
+            description = "Contact Info details that can be reached out in case of any issues"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountsContactInfoDto> getContactInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(accountsContactInfoDto);
+    }
 }
+
+
+
